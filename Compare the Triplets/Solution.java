867455f9d18bdb6894b027408909f69e9e8c8561
DilshanDilipudara <gunawardhanaudara@gmail.com>
2019-03-16 12:15:47 +0530

    Compare the Triplets

---
+++ Compare the Triplets/Solution.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        
+         ArrayList <Integer> c = new ArrayList <Integer>(2); 
+          int a1 = 0;
+          int b1 = 0;
+
+          for(int temp=0;temp<a.size();temp++){
+              if(a.get(temp) > b.get(temp)){
+                  a1++;
+              }
+              else if(b.get(temp)>a.get(temp)){
+                  b1++;
+              }
+          }
+              c.add(a1);
+              c.add(b1);
+          return  c;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = compareTriplets(a, b);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
